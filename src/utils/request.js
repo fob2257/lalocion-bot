43d6e27d85c508a775c6a3c740432b8455e522dc
Saul Meneses <fob2257@gmail.com>
2020-12-10 15:41:40 -0700

    WIP: Integrating TMDB API

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 const axios = require('axios').default;
 
 const omdbToken = process.env.OMDB_API_TOKEN;
+const tmdbToken = process.env.TMDB_API_KEY;
 
 const request = (baseURL) => (options) =>
   axios
@@ -10,5 +11,14 @@ const request = (baseURL) => (options) =>
 const omdbAPI = {
   request: request(`http://www.omdbapi.com/?apikey=${omdbToken}`)
 };
+// WIP
+const tmdbAPI = {
+  genresRequest: request(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${tmdbToken}`
+  ),
+  tmdbAPIGenresRequest: request(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${tmdbToken}`
+  )
+};
 
-module.exports = { omdbAPI };
+module.exports = { omdbAPI, tmdbAPI };
